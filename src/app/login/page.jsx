50b06b30b36e7a77dd02509d38b0df4a8492bdac
goodness5 <goodnesskolapo@gmail.com>
2023-06-03 08:36:52 +0100

    mobile responsiveness for side pane and login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,7 +10,7 @@ const Login = () => {
         <h3 className="text-[3em] mt-10">Login</h3>
         <p className="font-sans text-[1.4em] mt-3">Login to start managing your store.</p>
       </div>
-      <div className=" w-2/6 m-auto mt-8 justify-center items-center">
+      <div className="sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 m-auto mt-8 justify-center items-center">
         <form action="" className="flex flex-col justify-center items-center">
           <input
             type="email"
@@ -25,13 +25,23 @@ const Login = () => {
             placeholder="Password"
             id=""
             className="bg-black p-4 rounded-lg m-4 hover:bg-white w-full"
-          /><div className="-mt-4 justify-end w-full text-right">
-          <a href="settings/reset_password" className="text-right underline text-[#cb44f5]">Forgot Password?</a>
+          />
+          <div className="-mt-4 justify-end w-full text-right">
+            <a href="settings/reset_password" className="text-right underline text-[#cb44f5]">
+              Forgot Password?
+            </a>
           </div>
-          <button type="submit" className="bg-[#4b4bebec] text-white text-[1.4em] p-2 hover:bg-[#4b4bebfd] w-full rounded-xl m-auto mt-6">Login</button>
+          <button
+            type="submit"
+            className="bg-[#4b4bebec] text-white text-[1.4em] p-2 hover:bg-[#4b4bebfd] w-full rounded-xl m-auto mt-6"
+          >
+            Login
+          </button>
         </form>
         <div className="text-white justify-center items-center text-center mt-4 text-[1.1em]">
-          <p>Not registered yet?  <a href="#" className="text-right underline text-[#cb44f5] ml-3" >Request access</a></p>
+          <p>
+            Not registered yet? <a href="#" className="text-right underline text-[#cb44f5] ml-3">Request access</a>
+          </p>
         </div>
       </div>
     </div>
--- src/components/Sidepane.jsx
+++ src/components/Sidepane.jsx
@@ -1,20 +1,21 @@
-import { useEffect, useState } from 'react';
-import nugget from '../images/logo.png'
-import Image from 'next/image';
-import compare from '../images/compare.svg'
-import compsets from '../images/compsets.svg'
-import managestores from '../images/managestores.svg'
-import settings from '../images/settings.svg';
-import logout from '../images/logout.svg'
-import Link from 'next/link';
+import { useEffect, useState } from "react";
+import nugget from "../images/logo.png";
+import Image from "next/image";
+import compare from "../images/compare.svg";
+import compsets from "../images/compsets.svg";
+import managestores from "../images/managestores.svg";
+import settings from "../images/settings.svg";
+import logout from "../images/logout.svg";
+import Link from "next/link";
 
 const Sidepane = ({ activeTab, handleTabClick }) => {
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const renderTabClassName = (tabName) => {
-    return `mb-4 ${tabName === activeTab ? 'active border-l-2 border-blue-500' : ''}`;
+    return `mb-4 ${
+      tabName === activeTab ? "active border-l-2 border-blue-500" : ""
+    }`;
   };
-  
 
   const renderSubMenu = (parentTab, children) => {
     if (activeTab === parentTab) {
@@ -23,128 +24,224 @@ const Sidepane = ({ activeTab, handleTabClick }) => {
     return null;
   };
 
-//   useEffect(() => {
-//     setCurrentPath(window.location.pathname);
-//     const handleRouteChange = () => {
-//       setCurrentPath(window.location.pathname);
-//     };
-
-//     window.addEventListener('popstate', handleRouteChange);
-
-//     return () => {
-//       window.removeEventListener('popstate', handleRouteChange);
-//     };
-//   }, []);
-
   const getIcon = (tabName) => {
     // Map the tab names to their respective icons
     switch (tabName) {
-      case 'compare':
+      case "compare":
         return <Image src={compare} alt="Compare Icon" className="mr-2" />;
-      case 'compare-v2':
+      case "compare-v2":
         return <Image src={compare} alt="Compare V2 Icon" className="mr-2" />;
-      case 'comp-sets':
+      case "comp-sets":
         return <Image src={compsets} alt="Comp Sets Icon" className="mr-2" />;
-      case 'manage-stores':
-        return <Image src={managestores} alt="Manage Stores Icon" className="mr-2" />;
-      case 'settings':
+      case "manage-stores":
+        return (
+          <Image src={managestores} alt="Manage Stores Icon" className="mr-2" />
+        );
+      case "settings":
         return <Image src={settings} alt="Settings Icon" className="mr-2" />;
       default:
         return null;
     }
   };
 
-  return (
-   
-    <div className="bg-[#010716e5] text-white flex flex-col p-5 min-h-screen w-[20em]">
-        <div className='flex flex-row p-0 text-[2rem] align-middle items-center justify-center'>
-            <Image src={nugget} alt="" className=' w-16' />
-            <p className='p-2'>Nugget</p>
-        </div>
-        <p className='text-[#b3b2b25e] text-[1em]'>menu</p>
-      <ul className="list-none  font-semibold">
-        <li className={`mb-4 p-4 font-medium rounded-lg shadow-xl  ${renderTabClassName('compare')}`}>
-        <Link href="/compare" onClick={() => handleTabClick('compare')} className="flex items-center">
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-            {getIcon('compare')}
-            Compare
-          </Link>
-        </li>
-        <li className={`mb-4 p-4 font-medium rounded-lg shadow-xl ${renderTabClassName('compare-v2')}`}>
-          <a
-            href="/compare"
-            onClick={() => handleTabClick('compare-v2')}
-            className="flex"
-          >
-            {getIcon('compare-v2')}
-            Compare V2 [Beta]
-          </a>
-        </li>
-        <li className={`mb-4 p-4 font-medium rounded-lg shadow-xl ${renderTabClassName('competitive_sets')}`}>
-          <a
-            href="/competitive_sets"
-            onClick={() => handleTabClick('competitive_sets')}
-            className="flex items-center"
-          >
-            {getIcon('comp-sets')}
-            Comp Sets
-          </a>
-        </li>
-        <li className={`mb-4 p-4 font-medium rounded-lg shadow-xl ${renderTabClassName('manage-stores')}`}>
-          <a onClick={() => handleTabClick('manage-stores')} className="flex cursor-pointer">
-            {getIcon('manage-stores')}
-            Manage Stores
-          </a>
-          
-          {renderSubMenu('manage-stores', (
-            <div className='align-left ml-8'>
-              <ul className=' list-disc'>
-              <li className="font-medium mt-3">
-                <a href="/my-stores" onClick={() => handleTabClick('manage-stores')} className="font-medium mt-3">
-                  My Stores
-                </a>
-              </li>
-              <li className="font-medium mt-3">
-                <a href="/stores-to-monitor" onClick={() => handleTabClick('manage-stores')} className="font-medium mt-3">
-                  Stores to Monitor
-                </a>
-              </li>
-              <li className="font-medium mt-3">
-                <a href="/organization-stores" onClick={() => handleTabClick('manage-stores')} >
-                  Organization Stores
-                </a>
-              </li>
-              </ul>
-            </div>
-          ))}
-        </li>
-        <li className={`mb-4 p-4 ${renderTabClassName('settings')}`}>
-          <a onClick={() => handleTabClick('settings')} className="flex cursor-pointer">
-            {getIcon('settings')}
-            Settings
-          </a>
-          {renderSubMenu('settings', (
-            <li className='mt-4 list-disc ml-10'>
-              <a href="/settings/reset_password" onClick={() => handleTabClick('reset-password')} className="ml-4">
-                Reset Password
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsMenuOpen(true); // Open menu on desktop
+      } else {
+        setIsMenuOpen(false); // Close menu on mobile
+      }
+    };
+    window.addEventListener("click", setIsMenuOpen(false));
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial check on component mount
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isMenuOpen ? (
+        <div className="bg-[#010716e5] text-white  flex flex-col z-50 p-5 min-h-screen w-[20em] bg-opacity-60 sm:bg-opacity-0 absolute sm:relative">
+          <div className="top-0 right-2 absolute text-white flex sm:hidden">
+            <button className="" onClick={toggleMenu}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex flex-row p-0 text-[2rem] align-middle items-center justify-center">
+            <Image src={nugget} alt="" className="w-16" />
+            <p className="p-2">Nugget</p>
+          </div>
+          <p className="text-[#b3b2b25e] text-[1em]">menu</p>
+          <ul className="list-none  font-semibold">
+            <li
+              className={`mb-4 p-4 font-medium rounded-lg shadow-xl  ${renderTabClassName(
+                "compare"
+              )}`}
+            >
+              <Link
+                href="/compare"
+                onClick={() => handleTabClick("compare")}
+                className="flex items-center"
+              >
+                {getIcon("compare")}
+                Compare
+              </Link>
+            </li>
+            <li
+              className={`mb-4 p-4 font-medium rounded-lg shadow-xl ${renderTabClassName(
+                "compare-v2"
+              )}`}
+            >
+              <a
+                href="/compare"
+                onClick={() => handleTabClick("compare-v2")}
+                className="flex"
+              >
+                {getIcon("compare-v2")}
+                Compare V2 [Beta]
               </a>
             </li>
-          ))}
-        </li>
-      </ul>
-      <p className='text-[#b3b2b25e] text-[1em]'>Profile</p>
-      <div className='justify-between flex text-[0.9em] mt-6'>
-        <a href="docs/privacy_policy">Privacy policy</a>
-        <a href="docs/terms_of_use">Terms of Use</a>
-      </div>
-      <div className='m-auto w-3/4 flex flex-row bg-[#1a181863] rounded-lg mt-8'>
-      <a href="/login" className='flex w-full align-middle items-center text-center justify-center'>
-            <Image src={logout} alt="" />
-           <p className='p-2 text-[1em]'> Logout</p>
+            <li
+              className={`mb-4 p-4 font-medium rounded-lg shadow-xl ${renderTabClassName(
+                "competitive_sets"
+              )}`}
+            >
+              <a
+                href="/competitive_sets"
+                onClick={() => handleTabClick("competitive_sets")}
+                className="flex items-center"
+              >
+                {getIcon("comp-sets")}
+                Comp Sets
+              </a>
+            </li>
+            <li
+              className={`mb-4 p-4 font-medium rounded-lg shadow-xl ${renderTabClassName(
+                "manage-stores"
+              )}`}
+            >
+              <a
+                onClick={() => handleTabClick("manage-stores")}
+                className="flex cursor-pointer"
+              >
+                {getIcon("manage-stores")}
+                Manage Stores
+              </a>
+
+              {renderSubMenu(
+                "manage-stores",
+                <div className="align-left ml-8">
+                  <ul className="list-disc">
+                    <li className="font-medium mt-3">
+                      <a
+                        href="/my-stores"
+                        onClick={() => handleTabClick("manage-stores")}
+                        className="font-medium mt-3"
+                      >
+                        My Stores
+                      </a>
+                    </li>
+                    <li className="font-medium mt-3">
+                      <a
+                        href="/stores-to-monitor"
+                        onClick={() => handleTabClick("manage-stores")}
+                        className="font-medium mt-3"
+                      >
+                        Stores to Monitor
+                      </a>
+                    </li>
+                    <li className="font-medium mt-3">
+                      <a
+                        href="/organization-stores"
+                        onClick={() => handleTabClick("manage-stores")}
+                      >
+                        Organization Stores
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </li>
+            <li className={`mb-4 p-4 ${renderTabClassName("settings")}`}>
+              <a
+                onClick={() => handleTabClick("settings")}
+                className="flex cursor-pointer"
+              >
+                {getIcon("settings")}
+                Settings
+              </a>
+              {renderSubMenu(
+                "settings",
+                <li className="mt-4 list-disc ml-10">
+                  <a
+                    href="/settings/reset_password"
+                    onClick={() => handleTabClick("reset-password")}
+                    className="ml-4"
+                  >
+                    Reset Password
+                  </a>
+                </li>
+              )}
+            </li>
+          </ul>
+          <p className="text-[#b3b2b25e] text-[1em]">Profile</p>
+          <div className="justify-between flex text-[0.9em] mt-6">
+            <a href="docs/privacy_policy">Privacy policy</a>
+            <a href="docs/terms_of_use">Terms of Use</a>
+          </div>
+          <div className="m-auto w-3/4 flex flex-row bg-[#1a181863] rounded-lg mt-8">
+            <a
+              href="/login"
+              className="flex w-full align-middle items-center text-center justify-center"
+            >
+              <Image src={logout} alt="" />
+              <p className="p-2 text-[1em]"> Logout</p>
             </a>
-      </div>
+          </div>
+        </div>
+      ) : (
+        <button
+          className="mr-4 bg-[#131212] shadow-2xl text-white text-[2em] p-2 rounded-xl"
+          onClick={toggleMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 8h16M4 12h16M4 16h16M4 20h16"
+            />
+          </svg>
+        </button>
+      )}
     </div>
-  
   );
 };
 
