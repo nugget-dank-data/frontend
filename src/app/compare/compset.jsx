2b0728f81056c1cd3039deb3dedfe621d4f9e6cf
goodness5 <goodnesskolapo@gmail.com>
2023-06-06 12:20:23 +0100

    compset pop up fixed

---
+++ src/app/compare/compset.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Compsetprop = ({ closeFunction }) => {
+
+    const handleSubmit = () => {
+        closeFunction();
+        //to-do
+        //implement submit logic
+    }
+  return (
+    <div className="bg-white rounded-xl flex flex-col justify-center w-1/4 border p-6 absolute top-[50%] z-50 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+      <h2>Select a Comp-set</h2>
+
+      <button
+        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+        onClick={closeFunction}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-6 h-6"
+        >
+          <path d="M18 6L6 18M6 6l12 12" />
+        </svg>
+      </button>
+      <div className="flex flex-col justify-center items-center">
+      <select name="" id="" placeholder="comp-set" className="p-3 w-full active:border-none after:border-none rounded-lg m-4">
+        <option value="">{'' || 'compset1'}</option>
+        
+      </select>
+
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className="bg-[#0202a5] text-white w-full rounded-lg p-2 m-auto"
+      >
+        Select comp-set
+      </button>
+      <a href="#" className="mt-4 mb-4 text-[#8a206f]">View in accelerated full screen [beta]</a>
+      <p className="text-[#8a206f] cursor-pointer" onClick={closeFunction}>cancel</p>
+      </div>
+    </div>
+  );
+};
+
+export default Compsetprop;
--- src/app/compare/filters.jsx
+++ src/app/compare/filters.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import store from "../../images/stores.svg";
 import Image from "next/image";
 import droped from "../../images/g.svg";
@@ -8,6 +8,7 @@ import category from "../../images/category.svg";
 import brand from "../../images/brand.svg";
 import Results from "./results";
 import PriceRange from "@/components/PriceRange";
+import Compsetprop from './compset'
 
 const Filters = () => {
   const [range, setRange] = React.useState([0, 300]);
@@ -19,6 +20,8 @@ const Filters = () => {
   const [isStoreDropdownOpen, setIsStoreDropdownOpen] = useState(false);
   const [isCategoryDropdownOpen, setIsCategoryDropdownOpen] = useState(false);
   const [isSizeDropdownOpen, setIsSizeDropdownOpen] = useState(false);
+  const [showCompset, setShowCompset] = useState(false);
+  const compsetRef = useRef(null);
 
   useEffect(() => {
     // Fetch stores from API
@@ -55,6 +58,10 @@ const Filters = () => {
     setSelectedStore(store);
     setIsStoreDropdownOpen(false);
   };
+
+  const handlecompset = () => {
+    setShowCompset(!showCompset);
+  }
 
   function handlePriceChanges(event, newValue) {
     setRange(newValue);
@@ -93,132 +100,138 @@ const Filters = () => {
 
   return (
     <>
-    <div className="p-6 flex flex-col md:flex-row border-b"> 
-      <div className="store p-4 w-full">
-        <h1 className="text-[1.5em]">Store</h1>
-        <div
-          className="flex cursor-pointer justify-between w-3/4 p-2 items-center border rounded-lg bg-[#57545411] "
-          onClick={toggleStoreDropdown}
-        >
-          <Image src={store} alt="b" className="w-[1.4em]" />
-          <span className="ml-6 text-[#05050585]">
-            {selectedStore.name || "Search store ..."}
-          </span>
-          <Image
-            src={isStoreDropdownOpen ? undroped : droped}
-            className="w-4 ml-auto"
-          />
-        </div>
-        {isStoreDropdownOpen && (
-          <div className="bg-white text-black border w-full">
-            {stores.map((store) => (
-              <div
-                key={store.id}
-                className="cursor-pointer text-black p-4 hover:bg-gray-200"
-                onClick={() => handleStoreChange(store)}
-              >
-                {store.name}
-              </div>
-            ))}
-          </div>
-        )}
-        <div className="compset mt-5">
-          <a href="#" className="text-[#9c0195fd]">populate with comp-set</a>
-        </div>
-      </div>
-      <div className="filters w-full p-4">
-        <h1 className="text-[1.5em]">Filters</h1>
-        <div className="flex flex-col md:flex-row">
-          <div className="flex-col flex w-full md:w-1/2">
-            <div
-              className="flex mb-8 cursor-pointer mr-0 sm:mb-0 sm:mr-8 justify-between p-2 items-center border rounded-lg bg-[#57545431]"
-              onClick={toggleCategoryDropdown}
-            >
-              <Image src={category} alt="b" className="w-[1.4em]" />
-              <span className="ml-1 text-[#05050585]">
-                {selectedCategory.name || "Category"}
-              </span>
-              <Image
-                src={isCategoryDropdownOpen ? undroped : droped}
-                className="w-4 ml-auto"
-              />
-            </div>
-            {isCategoryDropdownOpen && (
-              <div className="bg-white text-black border w-full">
-                {stores.map((store) => (
-                  <div
-                    key={store.id}
-                    className="cursor-pointer text-black p-4 hover:bg-gray-200"
-                    onClick={() => handleCategoryChange(store.category)}
-                  >
-                    {store.category}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-          <div className="flex-col flex w-full md:w-1/2">
-            <div
-              className="flex cursor-pointer ml-0 sm:mb-0 sm:ml-8 justify-between p-2 items-center border rounded-lg bg-[#57545431]"
-              onClick={toggleSizeDropdown}
-            >
-              <Image src={category} alt="b" className="w-[1.4em]" />
-              <span className="ml-1 text-[#05050585]">
-                {selectedSize.name || "Size"}
-              </span>
-              <Image
-                src={isSizeDropdownOpen ? undroped : droped}
-                className="w-4 ml-auto"
-              />
-            </div>
-            {isSizeDropdownOpen && (
-              <div className="bg-white text-black border w-full">
-                {stores.map((store) => (
-                  <div
-                    key={store.id}
-                    className="cursor-pointer text-black p-4 hover:bg-gray-200"
-                    onClick={() => handleSizeChange(store.size)}
-                  >
-                    {store.size}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="brand border bg-[#57545431] rounded-lg flex m-auto w-2/4 p-2 mt-3">
-          <div className="relative flex-grow">
+      <div className="relative p-6 flex flex-col md:flex-row border-b">
+        <div className="store p-4 w-full">
+          <h1 className="text-[1.5em]">Store</h1>
+          <div
+            className="flex cursor-pointer justify-between w-3/4 p-2 items-center border rounded-lg bg-[#57545411] "
+            onClick={toggleStoreDropdown}
+          >
+            <Image src={store} alt="b" className="w-[1.4em]" />
+            <span className="ml-6 text-[#05050585]">
+              {selectedStore.name || "Search store ..."}
+            </span>
             <Image
-              src={brand}
-              alt="b"
-              className="absolute left-2 top-1/2 transform -translate-y-1/2"
+              src={isStoreDropdownOpen ? undroped : droped}
+              className="w-4 ml-auto"
             />
-            <input
-              type="text"
-              className="pl-8 bg-transparent outline-none text-[#0a0a0a83] w-full"
-              placeholder="Brand"
+          </div>
+          {isStoreDropdownOpen && (
+            <div className="bg-white text-black border w-full">
+              {stores.map((store) => (
+                <div
+                  key={store.id}
+                  className="cursor-pointer text-black p-4 hover:bg-gray-200"
+                  onClick={() => handleStoreChange(store)}
+                >
+                  {store.name}
+                </div>
+              ))}
+            </div>
+          )}
+          <div className="compset mt-5">
+            <button onClick={handlecompset} className="text-[#9c0195fd]">populate with comp-set</button>
+          </div>
+        </div>
+        <div className="filters w-full p-4">
+          <h1 className="text-[1.5em]">Filters</h1>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-col flex w-full md:w-1/2">
+              <div
+                className="flex mb-8 cursor-pointer mr-0 sm:mb-0 sm:mr-8 justify-between p-2 items-center border rounded-lg bg-[#57545431]"
+                onClick={toggleCategoryDropdown}
+              >
+                <Image src={category} alt="b" className="w-[1.4em]" />
+                <span className="ml-1 text-[#05050585]">
+                  {selectedCategory.name || "Category"}
+                </span>
+                <Image
+                  src={isCategoryDropdownOpen ? undroped : droped}
+                  className="w-4 ml-auto"
+                />
+              </div>
+              {isCategoryDropdownOpen && (
+                <div className="bg-white text-black border w-full">
+                  {stores.map((store) => (
+                    <div
+                      key={store.id}
+                      className="cursor-pointer text-black p-4 hover:bg-gray-200"
+                      onClick={() => handleCategoryChange(store.category)}
+                    >
+                      {store.category}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div className="flex-col flex w-full md:w-1/2">
+              <div
+                className="flex cursor-pointer ml-0 sm:mb-0 sm:ml-8 justify-between p-2 items-center border rounded-lg bg-[#57545431]"
+                onClick={toggleSizeDropdown}
+              >
+                <Image src={category} alt="b" className="w-[1.4em]" />
+                <span className="ml-1 text-[#05050585]">
+                  {selectedSize.name || "Size"}
+                </span>
+                <Image
+                  src={isSizeDropdownOpen ? undroped : droped}
+                  className="w-4 ml-auto"
+                />
+              </div>
+              {isSizeDropdownOpen && (
+                <div className="bg-white text-black border w-full">
+                  {stores.map((store) => (
+                    <div
+                      key={store.id}
+                      className="cursor-pointer text-black p-4 hover:bg-gray-200"
+                      onClick={() => handleSizeChange(store.size)}
+                    >
+                      {store.size}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="brand border bg-[#57545431] rounded-lg flex m-auto w-2/4 p-2 mt-3">
+            <div className="relative flex-grow">
+              <Image
+                src={brand}
+                alt="b"
+                className="absolute left-2 top-1/2 transform -translate-y-1/2"
+              />
+              <input
+                type="text"
+                className="pl-8 bg-transparent outline-none text-[#0a0a0a83] w-full"
+                placeholder="Brand"
+              />
+            </div>
+          </div>
+
+          <div className="price">
+            <PriceRange
+              range={range}
+              handlePriceChanges={handlePriceChanges}
             />
+          </div>
+
+          <div className="m-auto flex items-center justify-center mt-4">
+            <button type="submit" className="bg-[#12335a] m-auto p-2 pl-4 pr-4 rounded-lg text-white" onClick={applyFilters}>
+              Apply filters
+            </button>
           </div>
         </div>
 
-        <div className="price">
-         <PriceRange
-         range={range}
-         handlePriceChanges={handlePriceChanges}
-         />
-        </div>
-        
-        <div className="m-auto flex items-center justify-center mt-4">
-        <button type="submit" className="bg-[#12335a] m-auto p-2 pl-4 pr-4 rounded-lg text-white" onClick={applyFilters}>
-          Apply filters
-        </button>
-        </div>
       </div>
-
-    </div>
+      {showCompset && (
+        <div ref={compsetRef}>
+          <Compsetprop closeFunction={handlecompset} />
+        </div>
+      )}
       <Results storesData={storesData} />
-      </>
+    </>
   );
 };
+
 
 export default Filters;
