5042e5861b0b702876ff8dbbea319d72959b4aa5
goodness5 <goodnesskolapo@gmail.com>
2023-09-14 14:25:48 +0100

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,15 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./src/**/*.{html,js}",
-    'transform: (content) => content.replace(/taos:/g, "")',
-  ],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
   ],
   theme: {
     extend: {
       scrollbar: ['rounded-lg'],
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [
     require('tailwind-scrollbar-hide'),
     require('tailwind-scrollbar'),
-    require("tw-elements/dist/plugin.cjs"),
     // require('taos/plugin'),
   ],
 }
